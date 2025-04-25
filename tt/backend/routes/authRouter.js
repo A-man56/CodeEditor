90a59cad75664827edd9749320be81a67ab3ee2e
A-man56 <singhamanjeet87@gmail.com>
2025-04-26 01:06:21 +0530

    updated not major change

--- tt/backend/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const {signup,login} = require('../controllers/authController');
-const {signupValidation,loginValidation} = require('../middleware/authvalidation');
-
-router.post('/signup',signupValidation,signup);
-router.post('/login',loginValidation,login);
-
-
-module.exports = router;
